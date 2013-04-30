b73efd1e36ca744b3777f47c9d2e6f009a30aebe
Saravanan <saruios@gmail.com>
2013-05-01 04:24:32 +0530

    Very first commit

---
+++ SARUnArchiveANY/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SARUnArchiveANY
+//
+//  Created by Saravanan V on 26/04/13.
+//  Copyright (c) 2013 SARAVANAN. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SARUnArchiveANY/ViewController.m
@@ -0,0 +1,77 @@
+//
+//  ViewController.m
+//  SARUnArchiveANY
+//
+//  Created by Saravanan V on 26/04/13.
+//  Copyright (c) 2013 SARAVANAN. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "SARUnArchiveANY.h"
+#import "LZMAExtractor.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self unZip];
+    [self unRar];
+    [self Unzip7z];
+}
+
+- (void)unArchive: (NSString *)filePath destinationPath:(NSString *)destPath{
+    NSAssert(filePath, @"can't find filePath");
+    SARUnArchiveANY *unarchive = [[SARUnArchiveANY alloc]initWithPath:filePath];
+    unarchive.destinationPath = destPath;//(Optional). If it is not given, then the file is unarchived in the same location of its archive/file.
+    unarchive.completionBlock = ^(NSArray *filePaths){
+      NSLog(@"For Archive : %@",filePath);
+		for (NSString *filename in filePaths) {
+			NSLog(@"File: %@", filename);
+		}
+    };
+    unarchive.failureBlock = ^(){
+//        NSLog(@"Cannot be unarchived");
+    };
+    [unarchive decompress];
+}
+
+- (void)unZip{
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Zip_Example" ofType:@"zip"];
+    NSString *destPath = [self applicationDocumentsDirectory];
+    [self unArchive:filePath destinationPath:destPath];
+}
+
+- (void)unRar{
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"example" ofType:@"rar"];
+    NSString *destPath = [self applicationDocumentsDirectory];
+    [self unArchive:filePath destinationPath:destPath];
+}
+
+- (void)Unzip7z{
+	NSString *archiveFilename = @"example.7z";
+	NSString *archiveResPath = [[NSBundle mainBundle] pathForResource:archiveFilename ofType:nil];
+    NSAssert(archiveResPath, @"can't find .7z file");
+    NSString *destPath = [self applicationDocumentsDirectory];
+    [self unArchive:archiveResPath destinationPath:destPath];
+}
+
+- (NSString *) applicationDocumentsDirectory
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *basePath = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
+    return basePath;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
