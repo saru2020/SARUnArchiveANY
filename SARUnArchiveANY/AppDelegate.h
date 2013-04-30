a098e11911cf32f867677c2179071693d78d6bb9
Saravanan <saruios@gmail.com>
2013-05-01 05:18:45 +0530

    Commit all

---
+++ SARUnArchiveANY/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SARUnArchiveANY
+//
+//  Created by Saravanan V on 26/04/13.
+//  Copyright (c) 2013 SARAVANAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SARUnArchiveANY/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SARUnArchiveANY
+//
+//  Created by iLink on 4/26/13.
+//  Copyright (c) 2013 SARAVANAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
